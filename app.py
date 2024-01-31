c674557359e3cafea771d7216e664dabbe6b4c56
General202 <128303169+General202@users.noreply.github.com>
2024-01-31 18:34:53 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
 
+from sql_queries import BlogDB
+
 app = Flask(__name__)
 
+db = BlogDB("shop.db")
+
 @app.route("/")
-def hello_world():
-    return render_template("index.html")
+def index():
+    categories = db.get_all_categories()
+    posts = db.get_all_posts()
+    print(posts)
+    return render_template("index.html",
+                            title = "Сайт про види ноутбуків",
+                            posts = posts,
+                            categories = categories)
 
 @app.route("/logika")
 def hello_logika():
--- sql_queries.py
+++ sql_queries.py
@@ -1,38 +1,36 @@
 import sqlite3
 
-conn = sqlite3.connect("Artistc.db")
-cursor = conn.cursor() #створюєм курсор
+class BlogDB:
+    def __init__(self, dbname):
+        self.dbname = dbname
+        self.conn = None
+        self.cursor = None
 
-cursor.execute("SELECT * FROM artists")
+    def open(self):
+        self.conn = sqlite3.connect(self.dbname)
+        self.cursor = self.conn.cursor() #створюєм курсор
 
+    def close(self):
+        self.cursor.close()
+        self.conn.close()
 
-# Запитання 1. Інформація про скількох художників представлена у базі даних?
-data = cursor.fetchall()
-print(len(data))
+    def get_all_posts(self):
+        self.open()
+        self.cursor.execute("SELECT * FROM posts")
+        data = self.cursor.fetchall()
+        self.close()
+        return data
 
-# Запитання 2. Скільки жінок (Female) у базі?
-cursor.execute('''SELECT * FROM artists WHERE "Gender"=="Female" ''')
-data = cursor.fetchall()
-print(len(data))
+    def get_posts_by_category(self, category_id):
+        self.open()
+        self.cursor.execute("SELECT * FROM posts WHERE category_id = ?", [category_id])
+        data = self.cursor.fetchall()
+        self.close()
+        return data
 
-# Запитання 3. Скільки людей у базі даних народилися до 1900 року?
-cursor.execute('''SELECT * FROM artists WHERE "Birth Year"<1900 ''')
-data = cursor.fetchall()
-print(len(data))
-
-
-# Запитання 4*. Як звати найстаршого художника?
-cursor.execute('''SELECT * FROM artists ORDER BY "Birth Year" ''')
-data = cursor.fetchone()
-print(data[1])
-
-cursor.execute('''INSERT INTO artists("Artist ID", "Name", "Nationality", "Gender", "Birth Year")
-               VALUES( (?), (?), (?), (?), (?) )''', [650, "Ван Гог", "голандець", "Male", "1853"])
-
-cursor.execute('''INSERT INTO artists("Artist ID", "Name", "Nationality", "Gender", "Birth Year")
-               VALUES( (?), (?), (?), (?), (?) )''', [652, "Леонардо Да Вінчі", "італієць", "Male", "1893"])
-
-conn.commit()
-
-cursor.close()
-conn.close()
+    def get_all_categories(self):
+        self.open()
+        self.cursor.execute("SELECT * FROM categories")
+        data = self.cursor.fetchall()
+        self.close()
+        return data
