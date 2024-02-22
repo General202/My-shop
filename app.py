5768e77a8bbc84823c02e57aa351db2ab2231874
General202 <128303169+General202@users.noreply.github.com>
2024-02-22 21:57:15 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'jdkkfgnbdnbjkd' 
-db = BlogDB("shop.db")
+db = BlogDB("Shop.db")
 PATH = os.path.dirname(__file__) + os.sep
 
 @app.route("/")
